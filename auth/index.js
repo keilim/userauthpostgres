9ca2fcce1130e0d59f0bb4d6e05d6f885760a576
keilim <keilim@deloitte.com>
2019-01-02 16:37:36 -0500

    second

--- auth/index.js
+++ auth/index.js
@@ -7,9 +7,30 @@ router.get('/', (req, res) => {
 	res.json({message:'hey'})
 });
 
-router.post('/signup', (req, res) => {
-	res.json({
-		message: 'return'
-	});
+//Users can login to app with valid email/password
+// no blank missing meail
+// no blank or incorrect password
+function validUser(user) {
+	const validEmail = typeof user.email == 'string' && user.email.trim() != '';
+	const validPassword = typeof user.password == 'string' && 
+				user.password.trim() != '' &&
+				user.password.trim().length >= 6;
+	return validEmail && validPassword;
+}
+
+router.post('/signup', (req, res, next) => {
+	if (validUser(req.body)) {
+		User
+		    .getOneByEmail(req.body.email)
+		    .then(user => {
+			console.log('user', user);
+			res.json({
+				user,
+				message: 'return'
+			});
+		    });
+	} else  {
+		next(new Error('Invalid user'));
+	}
 });
 module.exports = router;
