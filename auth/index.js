1d8ed3715c5dc1474572578f1f1f12aa9de78675
keilim <keilim@deloitte.com>
2019-01-07 16:51:55 -0500

    progress

--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
 const User = require('../db/user');
@@ -12,10 +13,13 @@ router.get('/', (req, res) => {
 // no blank missing meail
 // no blank or incorrect password
 function validUser(user) {
+	console.log(user.email);
 	const validEmail = typeof user.email == 'string' && user.email.trim() != '';
 	const validPassword = typeof user.password == 'string' && 
 				user.password.trim() != '' &&
 				user.password.trim().length >= 6;
+	console.log(validEmail);
+	console.log(validPassword);
 	return validEmail && validPassword;
 }
 
@@ -28,10 +32,28 @@ router.post('/signup', (req, res, next) => {
 				//if user not found
 				if (!user) {
 					//this is a unique email
-					res.json({
-						user,
-						message: 'return'
-					});
+					//hash password
+					bcrypt.hash(req.body.password, 10)
+						.then((hash) => {
+							//store hash in your password
+							//insert user into db
+							const user = {
+								email: req.body.email,
+								password: hash,
+								created_at: new Date()
+							};
+
+							User
+								.create(user)
+								.then(id => {
+									//redirect
+									res.json({
+										id,
+										message: 'return'
+									});
+								})
+						});
+
 				} else {
 					//email in use
 					next(new Error('Email in use'));
@@ -41,4 +63,45 @@ router.post('/signup', (req, res, next) => {
 		next(new Error('Invalid user'));
 	}
 });
+
+router.post('/login', (req, res, next) => {
+	if (validUser(req.body)) {
+		// check to see if in DB
+		User
+			.getOneByEmail(req.body.email)
+			.then(user => {
+				console.log('user', user);
+				if (user) {
+					//compare password with hashed password
+					bcrypt
+						.compare(req.body.password, user.password)
+						.then((result) => {
+							// if the password matched
+							if (result) {
+								//makes it true when it is in development;
+								const isSecure = req.app.get('env') != 'development'; 
+								// setting the set-cookie' header
+								// also making it a secure cookie.
+								res.cookie('user_id', user.id, {
+									httpOnly: true,
+									secure: isSecure,
+									// signed hashes it
+									signed: true
+								});
+								res.json({
+									message: 'Logged in...'
+								});
+							} else {
+								next(new Error('Invalid Login'));
+							}
+						});
+				} else {
+					next(new Error('Invalid Login'));
+				}
+			});
+	} else  {
+		next(new Error('Invalid Login'));
+	}
+})
+
 module.exports = router;
